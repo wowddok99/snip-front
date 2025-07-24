4962d152aa52b59a45a7aa6cdcd01e52c5f87624
전상은 <wowddok99@gmail.com>
2025-07-24 17:48:59 +0900

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+import "./globals.css";
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en">
+            <body className={""}>
+            {children}
+            </body>
+        </html>
+    );
+}
